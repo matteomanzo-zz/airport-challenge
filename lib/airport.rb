cf51dce2b9d873f337d9bc53158682ae5a498884
matteomanzo <imatteo.manzo@gmail.com>
2014-12-13 15:43:21 +0000

    Planes cannot land if airport is full, test passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,6 +10,7 @@ class Airport
   end
 
   def dock(plane)
+    raise 'Airport is full!' if full?
     if plane.status == 'landed'
       @planes << plane
     else
@@ -26,4 +27,9 @@ class Airport
       @planes.delete(plane)
     end
   end
+
+  def full?
+    planes_count == @capacity
+  end
+
 end
